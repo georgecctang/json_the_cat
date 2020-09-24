b65199f62b15e905193c0102d81597f8f64c9cfe
George Tang <georgecctang@gmail.com>
2020-09-24 18:40:55 +0000

    changed api call to based on query search instead of id

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,24 @@
 // https://web.compass.lighthouselabs.ca/days/w02d4/activities/892
-
 // Prints cat breed description with fetching data from TheCatAPI
 
 const request = require('request');
 
 // Process user input
 // The id has 4 small letters 
-const breedInput = process.argv[2].slice(0,4).toLowerCase();
+const breedInput = process.argv[2];
 
-request.get(`https://api.thecatapi.com/v1/images/search?breed_id=${breedInput}`, (error, response, body) => {
+request.get(`https://api.thecatapi.com/v1/breeds/search?q=${breedInput}`, (error, response, body) => {
   if (error) {
     throw new Error(error);
   }
+  
+  // Parse string into JSON
   const data = JSON.parse(body);
-  // Returns empty array if breed not found
+  
+  // Returns empty array if breed not found  
   if (data.length === 0) {
-    console.log ('Breed not found. Please try again.');
-  } else {
-    console.log(data[0].breeds[0].description);
+      console.log ('Breed not found. Please try again.');
+    } else {
+      console.log(data[0].description);
   }
 }); 
